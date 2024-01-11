972cb0ea175e66870ac9f74412bfc53dba841a0e
AdrianSilvaNascimento <adrianfsf10@gmail.com>
2024-01-10 21:48:29 -0300

    move cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(env.PORT);
 }
 bootstrap();
